1aa8dcb66749e3c5b78374e15f77d87ee3d0bf35
Leo Gordon <ens-lg4@users.noreply.github.com>
2018-06-25 16:23:12 +0100

    an entry should access its own files via get_path

--- class_entry.py
+++ class_entry.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python3
 
+import os
 import utils
 
 class Entry:
     def __init__(self, entry_name, entry_path):
         self.entry_name = entry_name
         self.entry_path = entry_path
+
+
+    def get_path(self, filename=None):
+        if filename:
+            return os.path.join(self.entry_path, filename)
+        else:
+            return self.entry_path
+
 
     def call(self, function_name, given_arg_dict):
         """ Call a given function of a given entry and feed it with arguments from a given dictionary.
@@ -24,3 +33,7 @@ if __name__ == '__main__':
 
     p, q = foo_entry.call('foo', { 'alpha' : 100, 'beta' : 200, 'gamma' : 300, 'epsilon' : 500, 'lambda' : 7777 } )
     print("P_foo = {}, Q_foo = {}\n".format(p,q))
+
+    dir_path    = foo_entry.get_path()
+    file_path   = foo_entry.get_path('abracadabra.txt')
+    print("dir_path = {}, file_path = {}\n".format(dir_path, file_path))
